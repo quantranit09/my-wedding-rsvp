589b749d9f1432b6f664cdf1a79c8bbe4ad80d2b
Quan Tran <qtran1.contractor@adb.org>
2026-07-14 20:17:23 +0700

    remove redundant image

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,6 +2,7 @@ import {
   ArrowDown,
   ArrowLeft,
   ArrowRight,
+  CalendarPlus,
   Check,
   Copy,
   Menu,
@@ -16,6 +17,7 @@ export const CloseIcon = X;
 export const ArrowDownIcon = ArrowDown;
 export const ArrowLeftIcon = ArrowLeft;
 export const ArrowRightIcon = ArrowRight;
+export const CalendarPlusIcon = CalendarPlus;
 export const CheckIcon = Check;
 export const CopyIcon = Copy;
 export const PlayIcon = Play;
